b604125577434576f5a23b1897cb4391a87c1622
UHanczar <u.hanczar@gmail.com>
2018-12-09 14:56:26 +0300

    Initial commit

---
+++ frontend/components/Page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Meta from './Meta';
+import Header from './Header';
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
