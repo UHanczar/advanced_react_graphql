eb44e084ada22e56a4bdd3c7371c1765921733bc
UHanczar <u.hanczar@gmail.com>
2018-12-16 13:08:33 +0300

    Creating prisma and yoga graphql servers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+
+const createServer = require('./createServer.js');
+const db = require('./db.js');
+
+const server = createServer();
+
+// TODO: create express middlewale to handle cookies (CORS)
+// TODO: Use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, deets => console.log(`Server is now running on port http://localhost:${deets.port}`));
